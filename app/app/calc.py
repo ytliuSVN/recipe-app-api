f201a1b32b80e34ef849c159203e0ab1230951b6
Penny <enderman542@gmail.com>
2024-10-29 00:10:39 +0900

    add test_subtract_numbers

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Calculator functions
 
 def add(x, y):
     return x + y
+
+
+def subtract(x, y):
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -12,3 +12,8 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
