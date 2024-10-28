aa283e9e3f81dd402e97523c7a2e426a362cc981
Penny <enderman542@gmail.com>
2024-10-28 23:55:00 +0900

    fix lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,13 +1,13 @@
 """
 Sample tests
 """
-
 from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    
+
     def test_add_numbers(self):
         res = calc.add(5, 6)
 
