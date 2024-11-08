e46a7e541393f2f528dac9cd6aa3e90a8877cfb8
Penny <enderman542@gmail.com>
2024-11-08 22:05:00 +0900

    require email input

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,3 +25,7 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
